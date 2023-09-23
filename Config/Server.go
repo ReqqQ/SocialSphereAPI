de8e1a935bcb8867a801a99cefde3a7883c22382
Patryk <nevvgaming@gmail.com>
2023-09-23 17:10:25 +0200

    deploy

---
+++ Config/Server.go
@@ -0,0 +1,19 @@
+package Config
+
+import (
+	"SocialSphereAPI/Config/DI"
+)
+
+func Init() *fiber.App {
+	_, err := DI.InitDIContainer()
+	if err != nil {
+		return nil
+	}
+	err = godotenv.Load(".env")
+
+	return fiber.New()
+}
+
+func Start(app *fiber.App) {
+	app.Listen(":3000")
+}
