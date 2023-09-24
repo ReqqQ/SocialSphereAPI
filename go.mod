6646783eea33842d03cd53c3841f23f61313be35
Patryk <nevvgaming@gmail.com>
2023-09-24 22:59:45 +0200

    - Base logic with go routines

--- go.mod
+++ go.mod
@@ -10,17 +10,29 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/cache/v9 v9.0.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/google/subcommands v1.0.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/nitishm/go-rejson v2.0.0+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/redis/go-redis/v9 v9.2.0 // indirect
+	github.com/redis/rueidis v1.0.18 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/tools v0.0.0-20190422233926-fe54fb35175b // indirect
+	golang.org/x/tools v0.9.3 // indirect
 )
--- src/ui/Routes.go
+++ src/ui/Routes.go
@@ -1,18 +1,100 @@
 package ui
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/cache/v9"
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 type Controllers struct {
 }
 
+type UsersList struct {
+	Ids []string
+}
+
+type User struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	City string `json:"city"`
+}
+
+func InitDBData() {
+	for {
+		ctx := context.Background()
+
+		client := redis.NewClient(&redis.Options{
+			Addr:     "127.0.0.1:6379",
+			Password: "127.0.0.1:6379",
+			DB:       0,
+			PoolSize: 50,
+		})
+		defer client.Close()
+
+		pipe := client.Pipeline()
+		usersIds := pipe.LRange(ctx, "users", int64(0), int64(-1))
+
+		_, err := pipe.Exec(ctx)
+		if err != nil {
+			fmt.Println("Błąd podczas wykonania pipeline:", err)
+		}
+
+		rawUsers, err := usersIds.Result()
+		if err != nil {
+			fmt.Println("Błąd podczas odczytywania wyniku GET:", err)
+		}
+		usersList := UsersList{Ids: rawUsers}
+		var usersPreparedList []*redis.StringCmd
+		for _, userId := range usersList.Ids {
+			usersPreparedList = append(usersPreparedList, pipe.Get(ctx, "user:"+userId))
+		}
+		pipe.Exec(ctx)
+		cacheRedisClient := redis.NewClient(&redis.Options{
+			Addr: "127.0.0.1:6379",
+		})
+		cacheClient := cache.New(&cache.Options{Redis: cacheRedisClient})
+		for _, rawData := range usersPreparedList {
+			rawUser, _ := rawData.Result()
+
+			var user User
+			err = json.Unmarshal([]byte(rawUser), &user)
+			if err != nil {
+				fmt.Println("Błąd podczas deserializacji JSON:", err)
+			}
+
+			cacheClient.Set(&cache.Item{
+				Ctx:   ctx,
+				Key:   "user:" + user.Id,
+				Value: user,
+				TTL:   1800,
+			})
+		}
+
+		time.Sleep(30 * time.Second)
+	}
+}
+
 func (co Controllers) InitGetRoutes(app *fiber.App) {
+	go InitDBData()
 	v1 := app.Group("/api/v1", func(c *fiber.Ctx) error {
 		c.Set("Version", "v1")
 		return c.Next()
 	})
 	v1.Get("/user/1", func(c *fiber.Ctx) error {
-		return c.JSON("test")
+		redisClient := redis.NewClient(&redis.Options{
+			Addr: "127.0.0.1:6379",
+		})
+
+		cacheClient := cache.New(&cache.Options{Redis: redisClient})
+
+		var userCache User
+		cacheClient.Get(context.Background(), "user:1", &userCache)
+
+		return c.JSON(userCache)
 	})
 }
